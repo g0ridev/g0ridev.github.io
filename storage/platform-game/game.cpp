c332f0ca236ed955cc21370dd75a7fb97c560dd9
g0ridev <buckie9777@gmail.com>
2025-10-01 15:58:44 -0500

    fixed the quiz. the json stuff still shows need to fix later.

--- storage/platform-game/game.cpp
+++ storage/platform-game/game.cpp
@@ -1,43 +1,124 @@
+
 #include "raylib.h"
 
-int main() {
-    // Initialize window
+int main(void)
+{
     const int screenWidth = 800;
     const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "My First Raylib Game!");
+
+    float playerX = 100.0;
+    float playerY = 800.0;
+    float playerWidth = 50.0;
+    float playerHeight = 50.0;
+    float startX = 100.0;
+    float startY = 800.0;
+
+    float enemyX = 300.0;
+    float enemyY = 200.0;
+    float enemyWidth = 50.0;
+    float enemyHeight = 50.0;
+
+
+    float groundY = 500.0;
+    float groundHeight = 100.0;
+    float velocityX = 0.0;
+    float velocityY = 0.0;
+    float jumpPower = -400.0;
+    float gravity = 980.0;
     
-    // Game variables
-    Vector2 ballPosition = { screenWidth/2, screenHeight/2 };
-    Vector2 ballSpeed = { 5.0f, 4.0f };
-    int ballRadius = 20;
-    
-    SetTargetFPS(60);
-    
-    // Main game loop
-    while (!WindowShouldClose()) {
-        // Update
-        ballPosition.x += ballSpeed.x;
-        ballPosition.y += ballSpeed.y;
-        
-        // Bounce off walls
-        if (ballPosition.x >= (screenWidth - ballRadius) || ballPosition.x <= ballRadius) {
-            ballSpeed.x *= -1.0f;
+    bool isOnGround = false;
+    float deathTimer = 0.5;
+
+
+
+
+
+
+    InitWindow(screenWidth, screenHeight, "first-platformer");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        float movementSpeed = 300.0;
+        float deltaTime = GetFrameTime();
+
+
+        if(IsKeyDown(KEY_LEFT)){
+            playerX -= movementSpeed * deltaTime;
         }
-        if (ballPosition.y >= (screenHeight - ballRadius) || ballPosition.y <= ballRadius) {
-            ballSpeed.y *= -1.0f;
+        if(IsKeyDown(KEY_RIGHT)){
+            playerX += movementSpeed * deltaTime;
+        }
+
+
+
+
+        if(IsKeyDown(KEY_SPACE)){
+            playerY += jumpPower * deltaTime;
+            isOnGround = false;
+        }
+
+        velocityY += gravity * deltaTime;
+
+        playerX += velocityX * deltaTime;
+        playerY += velocityY * deltaTime;
+
+
+
+        if(playerX < 0){
+            playerX = 0;
+            velocityX = 0;
+        }
+        if(playerX + playerWidth > screenWidth){
+            playerX = screenWidth - playerWidth;
+            velocityX = 0;
+        }
+        if(playerY < 0){
+            playerY = 0;
+            velocityY = 0;
+        }
+        if(playerY + playerHeight > screenHeight - groundHeight){
+            playerY = (screenHeight - groundHeight) - playerHeight;
+            velocityY = 0;
+            isOnGround = true;
+        }
+        else{
+            isOnGround = false;
+        }
+
+
+        if(playerX < enemyX + enemyWidth && playerX + playerWidth > enemyWidth && playerY < enemyY + enemyHeight && playerHeight > enemyHeight + enemyY){
+            playerX = startX;
         }
         
-        // Draw
+
+
+
+
+
+        
+
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        
-        DrawCircleV(ballPosition, ballRadius, MAROON);
-        DrawText("My First Game!", 10, 10, 20, DARKGRAY);
-        DrawFPS(10, 40);
-        
+
+            ClearBackground(SKYBLUE);
+
+            DrawRectangle(playerX,playerY,playerWidth,playerHeight,YELLOW);
+            DrawRectangle(enemyX,enemyY,enemyWidth,enemyHeight,RED);
+
+            DrawRectangle(0,groundY,screenWidth,groundHeight,GREEN);
+
+
+
+            DrawText("Platformer", 400, 400, 20, BLACK);
+
         EndDrawing();
     }
+
+    CloseWindow();       
     
-    CloseWindow();
+
+    
     return 0;
 }
